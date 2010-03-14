15929c2ebe4127ae8fad2f1257ff25755ca06fd2
Brad Fitzpatrick <brad@danga.com>
2010-03-14 12:20:16 -0500

    home page text, css, start of models

--- main.py
+++ main.py
@@ -16,20 +16,64 @@
 #
 
 
+import os
+
+from google.appengine.api import users
+from google.appengine.ext import db
 from google.appengine.ext import webapp
+from google.appengine.ext.webapp import template
 from google.appengine.ext.webapp import util
 
 
+class User(db.Model):
+  """A user's global state, not specific to a project."""
+  user = db.UserProperty(required=True)
+  url = db.StringProperty()
+  last_login = db.DateProperty()
+
+
+class Project(db.Model):
+  """A project which can be contributed to, with its metadata."""
+  name = db.StringProperty(required=True)  # primary key
+  pretty_name = db.StringProperty(required=False)
+  # main URL (String)
+  # main version control (String)
+  # how to send patches (text box)
+  # Owners (list/map User -> LastSeen, involvement level)
+
+
+class Contributor(db.Model):
+  """A user-project tuple."""
+  user = db.ReferenceProperty(User, required=True)
+  project = db.ReferenceProperty(Project, required=True)
+
+  is_active = db.BooleanProperty()
+  role = db.StringProperty()  # e.g. "Founder" freeform.
+  
+  
+
+
+  
 class MainHandler(webapp.RequestHandler):
 
   def get(self):
-    self.response.out.write('Hello world!')
+    path = os.path.join(os.path.dirname(__file__), 'index.html')
+    template_values = {
+      "foo": "bar",
+    }
+    self.response.out.write(template.render(path, template_values))
 
 
 class SiteHandler(webapp.RequestHandler):
 
   def get(self):
     self.response.out.write("I'm a site admin page.")
+
+
+class CreateHandler(webapp.RequestHandler):
+
+  def get(self):
+    self.response.out.write("Create page.")
 
 
 class ProjectHandler(webapp.RequestHandler):
@@ -41,6 +85,7 @@ class ProjectHandler(webapp.RequestHandler):
 def main():
   application = webapp.WSGIApplication([
       ('/', MainHandler),
+      ('/s/create', CreateHandler),
       ('/s/.*', SiteHandler),
       (r'/([a-z][a-z0-9_\.\-]*[a-z0-9])/?', ProjectHandler),
       ],
