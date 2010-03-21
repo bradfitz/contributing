b8642fc5fef9a4a0bf8f12952ed9e131e1e0026e
Brad Fitzpatrick <brad@danga.com>
2010-03-21 10:13:48 -0700

    More permissive URL regexp.

--- filters.py
+++ filters.py
@@ -8,7 +8,7 @@ def linkify(text):
   if not text:
     return ""
   text = text.replace('<', '&lt;').replace('>', '&gt;').replace("\n", '<br/>\n')
-  text = re.sub(r'\b(https?://[\w\-\/\?\&\=\.]+)',
+  text = re.sub(r'\b(https?://[\w\-\/\?\&\=\.\:\%\#]+)',
                 lambda x: "<a href='%s'>%s</a>" % (x.group(1), x.group(1)),
                 text)
   return text
